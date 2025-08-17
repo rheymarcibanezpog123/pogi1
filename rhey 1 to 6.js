9b8c3b7296af6a35537a416082a2a846dce7abf6
rheymarcibanezpog123 <rheymarc.ibanez@hcdc.edu.ph>
2025-08-17 14:13:16 +0800

    Update and rename rhey8.js to rhey 1 to 6.js

---
+++ rhey 1 to 6.js
@@ -0,0 +1,21 @@
+var name = "Rhey Marc";
+
+let age = "23";
+
+const PI = 3.1416;
+
+let isStudent = parseInt(age) < 25;
+
+let person = {
+  firstName: "Rhey",
+  lastName: "Marc",
+  age: 23,
+  address: {
+    country: "Philippines",
+    region: "Mindanao",
+    city: "Davao City"
+  },
+  hobbies: ["coding", "gaming", "reading"]
+};
+
+let colors = ["blue", "red", "green", "yellow", "blue", "black"];
